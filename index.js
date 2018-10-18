d17164a9112e1cccd7588fb02b6eabb44f57bcbc
Ryan Daley <rdaley321@yahoo.com>
2018-10-18 12:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ function currentLine(current) {
   let message = "The line is currently: ";
   for(let i=0; i<current.length; i++) {
     if(i === current.length - 1) {
-      message += `${i+1}. ${current[i]}`;
+     message += `${i+1}. ${current[i]}`;
+     return message;
     }
     message += `${i+1}. ${current[i]}, `;
   }
