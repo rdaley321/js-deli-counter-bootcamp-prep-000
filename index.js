c620f94a664946f5a528f13d9938b793b0b2d481
Ryan Daley <rdaley321@yahoo.com>
2018-10-18 12:01:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function takeANumber(n, name) {
-  return `Welcome, ${name}. You are number ${n} in line.`;
+  return `Welcome, ${name}. You are number ${n.length + 1} in line.`;
+}
+
+function nowServing(current) {
+  if(current.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  let nowServing = "";
+  nowServing = current[0];
+  current.shift();
+  return nowServing;
+}
+
+function currentLine(current) {
+  if(current.length === 0) {
+    return "The line is currently empty.";
+  }
+  let message = "The line is currently: ";
+  for(let i=0; i<current.length; i++) {
+    message += `${i+1}. ${current[i]}`;
+  }
+  return message;
 }
